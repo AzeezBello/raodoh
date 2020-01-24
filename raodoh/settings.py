33b37b87c872c759779eafed4f263a1d419b2b56
AzeezBello <azeez@scholarx.co>
2020-01-24 11:48:38 +0100

    env setup

--- raodoh/settings.py
+++ raodoh/settings.py
@@ -70,13 +70,13 @@ ROOT_URLCONF = 'raodoh.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'BACKEND': 'django.templates.backends.django.DjangoTemplates',
         'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
+                'django.templates.context_processors.debug',
+                'django.templates.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'social_django.context_processors.backends',
--- raodoh/urls.py
+++ raodoh/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+admin.site.site_header = 'Raodoh admin'
+admin.site.site_title = 'Raodoh admin'
+admin.site.index_title = 'Raodoh administration'
+admin.empty_value_display = '**Empty**'
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include('home.urls')),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
