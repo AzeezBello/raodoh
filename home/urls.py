6ac2a5587ab1f171a72b1ca9d860ce924bb91c44
AzeezBello <azeez@scholarx.co>
2020-02-07 16:12:58 +0100

    cleaning

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
     path('thanks/', views.thanks, name='thanks'),
+    path('profile/', views.profile, name='profile'),
     path('upload/', views.upload, name='upload'),
     path('activation_sent/', views.activation_sent, name='activation_sent'),
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
