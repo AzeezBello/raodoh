6ad370a543ab8d4d1bcdf32d9c24183f89013786
AzeezBello <azeez@scholarx.co>
2020-02-09 14:20:41 +0100

    optimization

--- members_area/admin.py
+++ members_area/admin.py
@@ -3,21 +3,24 @@ from .models import Course, Lesson, Comment, Category
 
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [' title', 'create_time']
+
+
 class CourseAdmin(admin.ModelAdmin):
-    pass
+    list_display = [' title', 'category', 'create_time']
 
 
 class LessonAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['title', 'course', 'video_url', 'create_time', 'last_modified']
 
 
 class CommentAdmin(admin.ModelAdmin):
     pass
 
 
-admin.site.register(Category, CommentAdmin)
-admin.site.register(Comment, CommentAdmin)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
-
+admin.site.register(Comment, CommentAdmin)
 
